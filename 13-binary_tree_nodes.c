806bfe0f247c2a176f8d73893dd60d2d5f76e8ca
Oloishorua <jacoboloishorua@gmail.com>
2024-03-28 01:57:21 +0300

    Task 13 -correction

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -14,3 +14,36 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	return (binary_tree_size(tree) - binary_tree_leaves(tree));
 }
+
+/**
+ * binary_tree_leaves - counts leaves in abin tree
+ * @tree: bin tree to count leaves
+ * Return: leaves number, 0 if NULL
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+        if (!tree)
+        {
+                return (0);
+        }
+
+        if (!tree->left && !tree->right)
+        {
+                return (1);
+        }
+
+        return (binary_tree_leaves(tree->right) + binary_tree_leaves(tree->left));
+}
+
+/**
+ * binary_tree_size - measures size of bin tree
+ * @tree: bin tree for size to be measured
+ * Return: size, 0 if NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+        if (!tree)
+                return (0);
+
+        return (binary_tree_size(tree->right) + binary_tree_size(tree->left) + 1);
+}
